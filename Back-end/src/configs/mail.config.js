24b6acf0ab4ad87f6b9c146e8097b0c2f1a1f39b
Phanvanbinh2000 <70782594+Phanvanbinh2000@users.noreply.github.com>
2022-10-24 18:06:46 +0700

    Update mail.config.js

--- Back-end/src/configs/mail.config.js
+++ Back-end/src/configs/mail.config.js
@@ -22,7 +22,7 @@ const sendEmail = async ({ to, subject, text, html, ...rest }) => {
       //config mail
       const mail = {
         //sender access
-        from: '"TTB Store" <no-reply@accounts.ttb-store.com>',
+        from: '"Mango" <no-reply@accounts.Mango.com>',
         //receiver access
         to,
         //subject
@@ -47,7 +47,7 @@ const sendEmail = async ({ to, subject, text, html, ...rest }) => {
 };
 
 const headerHtmlMail = `<h1 style="color: #4c649b; font-size: 48px; border-bottom: solid 2px #ccc;padding-bottom: 10px">
-      TTB Store<br />
+Mango<br />
     </h1>`;
 const footerHtmlVerifyMail = `<h3 style="color: red">
         Chú ý: Không đưa mã này cho bất kỳ ai,
@@ -62,8 +62,8 @@ const htmlSignupAccount = (token) => {
     ${headerHtmlMail}
     <h2 style="padding: 10px 0; margin-bottom: 10px;">
         Xin chào anh (chị),<br />
-        Mã xác nhận đăng ký tài khoản cho website TTB Store của anh (chị).<br />
-        Cảm ơn vì đã ghé thăm TTB Store <3
+        Mã xác nhận đăng ký tài khoản cho website Mango của anh (chị).<br />
+        Cảm ơn vì đã ghé thăm Mango <3
     </h2>
     <h3 style="background: #eee;padding: 10px;">
       <i><b>${token}</b></i>
@@ -78,7 +78,7 @@ const htmlResetPassword = (token) => {
     ${headerHtmlMail}
     <h2 style="padding: 10px 0; margin-bottom: 10px;">
         Xin chào anh (chị),<br />
-        Cửa hàng TTB Store đã nhận được yêu cầu lấy lại mật khẩu từ quý khách.<br />
+        Cửa hàng Mango đã nhận được yêu cầu lấy lại mật khẩu từ quý khách.<br />
         Đừng lo lắng, hãy nhập mã này để khôi phục:
     </h2>
     <h1 style="background: #eee;padding: 10px;">
@@ -97,7 +97,7 @@ const htmlWarningLogin = () => {
         Cửa hàng nghi ngờ có ai đó đã cố gắng đăng nhập vào tài khoản của quý khách.<br />
         Nếu quý khác không nhớ mật khẩu hãy nhấn vào "Quên mật khẩu" để lấy lại mật khẩu<br/>
     </h2>
-    <h1>Cảm ơn.</h1>
+    <h1>Mango Cảm ơn.</h1>
   </div>`;
 };
 
