db31d3009bdd0fbc4cc6e92250627c114f8ed1b8
nguyenvanthuy1302 <85467140+nguyenvanthuy1302@users.noreply.github.com>
2022-11-06 13:52:53 +0700

    Message

--- Back-end/src/constants/index.js
+++ Back-end/src/constants/index.js
@@ -3,7 +3,7 @@ module.exports = {
   MAX_SIZE_JSON_REQUEST: '25mb',
 
   // số lượng của mã xác thực
-  NUMBER_VERIFY_CODE: 6000,
+  NUMBER_VERIFY_CODE: 6,
 
   // thời gian tồn tại của mã xác thực
   VERIFY_CODE_TIME_MILLISECONDS: 10 * 60 * 1000,
