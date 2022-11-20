60ddd94d2b6d782a552a4744acd7c5f3f83dba3d
nguyenvanthuy1302 <85467140+nguyenvanthuy1302@users.noreply.github.com>
2022-11-20 18:37:51 +0700

    Update index.js

--- Front-end/src/containers/HomePage/SaleOff/index.js
+++ Front-end/src/containers/HomePage/SaleOff/index.js
@@ -5,11 +5,11 @@ import './index.scss';
 // Do cả chương trình chỉ có 1 list carousel
 // Nên lưu thẳng vào đây để đỡ tốn chi phí query
 const list = [
-  'https://res.cloudinary.com/mang013022002/image/upload/v1667910931/SaleOff/carousels/a1_x0o1xm.png',
+  'https://res.cloudinary.com/mang013022002/image/upload/v1668412964/SaleOff/carousels/a1_qzvez4.png',
   'https://res.cloudinary.com/mang013022002/image/upload/v1667805799/SaleOff/carousels/iphone-13-pro-max-case_o3d7t6.jpg',
-  'https://res.cloudinary.com/mang013022002/image/upload/v1667911124/SaleOff/carousels/iphone-12-banner-1_jnkyvn_1_vslspy.png',
+  'https://res.cloudinary.com/mang013022002/image/upload/v1668413465/SaleOff/carousels/iphone-12-banner-1_jnkyvn_2_ocjtpb.png',
   'https://res.cloudinary.com/mang013022002/image/upload/v1666770395/SaleOff/carousels/popupimage-1676288705_da5ys9.jpg',
-  'https://res.cloudinary.com/mang013022002/image/upload/v1667555786/SaleOff/carousels/carytp67u01_yu0nf7.webp',
+  'https://res.cloudinary.com/mang013022002/image/upload/v1668413786/SaleOff/carousels/sale11_w8yxdi.png',
   'https://res.cloudinary.com/mang013022002/image/upload/v1667555786/SaleOff/carousels/carcrycbe02_rpqqxv.webp',
   'https://res.cloudinary.com/mang013022002/image/upload/v1667555785/SaleOff/carousels/carvhqqe103_vjlcrq.webp',
   'https://res.cloudinary.com/mang013022002/image/upload/v1667555786/SaleOff/carousels/carousels04_ugkxff.webp',
